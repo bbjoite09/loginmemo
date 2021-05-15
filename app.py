6e304ad5a0521959df68370b4457de834ab0742b
Heeeon <bbjoite09@naver.com>
2021-05-15 13:39:42 +0900

    메모장 저장할 때 id 함께 저장

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import hashlib
 import datetime
+import hashlib
 import os
 
 import jwt
@@ -15,18 +15,90 @@ app = Flask(__name__)
 
 # mongodb 추가
 client = MongoClient('localhost', 27017)
-db = client.get_database('person')
+db = client.get_database('homepage')
 
 # .env 파일 읽어와 환경변수로 추가
 load_dotenv()
 # load_dotenv() 설정
 JWT_SECRET = os.environ['JWT_SECRET']
 
+
 # API 추가
 @app.route('/', methods=['GET'])  # 데코레이터 문법
 def index():  # 함수 이름은 고유해야 한다
-    memos = list(db.articles.find({}, {'_id': False}))
+    token = request.cookies.get('loginToken')
+
+    if token:
+        try:
+            payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+            id = payload['id']
+            memos = list(db.articles.find({'id': id}, {'_id': False}))
+        except jwt.ExpiredSignatureError:
+            memos = []
+    else:
+        memos = []
+
     return render_template('index.html', test='테스트', memos=memos)
+
+
+# 로그인
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+
+# 가입
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+
+# 가입 API
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    id = request.form['id_give']
+    pw = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw.encode()).hexdigest()
+    db.user.insert_one({'id': id, 'pw': pw_hash})
+
+    return jsonify({'result': 'success'})
+
+
+# 로그인 API
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    id = request.form['id_give']
+    pw = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw.encode()).hexdigest()
+    user = db.user.find_one({'id': id, 'pw': pw_hash})
+
+    # 로그인 성공 시 JWT 리턴
+    if user:
+        expiration_time = datetime.timedelta(hours=1)
+        payload = {
+            'id': id,
+            # JWT 유효 기간 - 이 시간 이후에는 JWT 인증이 불가능합니다.
+            'exp': datetime.datetime.utcnow() + expiration_time,
+        }
+        token = jwt.encode(payload, JWT_SECRET)
+        return jsonify({'result': 'success', 'token': token})
+
+    return jsonify({'result': 'fail', 'msg': '아이디 비밀번호 불일치'})
+
+
+@app.route('/user', methods=['POST'])
+def user():
+    token_receive = request.headers['authorization']
+    token = token_receive.split()[1]
+    print(token)
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        id = payload['id']
+        return jsonify({'result': 'success', 'id': id})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
 
 
 # 아티클 추가 API
@@ -35,34 +107,44 @@ def save_memo():
     form = request.form
     url_receive = form['url_give']
     comment_receive = form['comment_give']
+    token_receive = request.headers['authorization']
+    token = token_receive.split()[1]
+    print(token)
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    response = requests.get(
-        url_receive,
-        headers=headers
-    )
-    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        id = payload['id']
 
-    title = soup.select_one('meta[property="og:title"]')
-    url = soup.select_one('meta[property="og:url"]')
-    image = soup.select_one('meta[property="og:image"]')
-    description = soup.select_one('meta[property="og:description"]')
-    print(title['content'])
-    print(url['content'])
-    print(image['content'])
-    print(description['content'])
-    document = {
-        'title': title['content'],
-        'image': image['content'],
-        'description': description['content'],
-        'url': url['content'],
-        'comment': comment_receive,
-    }
-    db.articles.insert_one(document)
-    return jsonify(
-        {'result': 'success', 'msg': '저장했습니다.'}
-    )
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        response = requests.get(
+            url_receive,
+            headers=headers
+        )
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        title = soup.select_one('meta[property="og:title"]')
+        url = soup.select_one('meta[property="og:url"]')
+        image = soup.select_one('meta[property="og:image"]')
+        description = soup.select_one('meta[property="og:description"]')
+        print(title['content'])
+        print(url['content'])
+        print(image['content'])
+        print(description['content'])
+        document = {
+            'title': title['content'],
+            'image': image['content'],
+            'description': description['content'],
+            'url': url['content'],
+            'comment': comment_receive,
+            'id': id,
+        }
+        db.articles.insert_one(document)
+        return jsonify(
+            {'result': 'success', 'msg': '저장했습니다.'}
+        )
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
 
 
 @app.route('/memo', methods=['GET'])
@@ -76,80 +158,11 @@ def list_memo():
     return jsonify(result)
 
 
-@app.route('login', methods=['GET'])
-def login():
-    return render_template('login.html')
-
-
-@app.route('register', methods=['GET'])
-def register():
-    return render_template('register.html')
-
-
-@app.route('/api/login', methods=['GET'])
-def api_login():
-    id = request.form['id_give']
-    pw = request.form['pw_give']
-
-    # TODO id, pw 검증 후에 JWT 만들어서 리턴
-    pw_hash = hashlib.sha256(pw.encode()).hexdigest()
-
-    user= db.users.find_one({'id' : id, 'pw' : pw_hash}, {'_id' : False})
-    # 만약 가입했다면?
-    if user:
-        # 로그인 성공 - JWT 생성
-        expiration_time= datetime.timedelta(hours=1)
-        payload ={
-            'id' : id,
-            # 발급시간으로부터 1시간동안 jwt 유효
-            'exp' : datetime.datetime.utcnow() + expiration_time
-        }
-        token = jwt.encode(payload, JWT_SECRET)
-
-        return jsonify({'result' : 'succes', 'token':token})
-    else :
-        # 가입하지 않은 상태
-        return jsonify({'result': 'fail', 'msg' : '로그인 실패'})
-
-
-@app.route('/api/register', methods=['GET'])
-def api_register():
-    id = request.form['id_give']
-    pw = request.form['pw_give']
-
-    # salting
-    # 1. pw + 랜덤 문자열 추가(솔트)
-    # 솔트 추가된 비밀번호를 해시
-    # DB에 저장할때는(해시 결과물 + 적절한 솔트) 묶어서 저장
-
-    # 회원가입
-    pw_hash = hashlib.sha256(pw.encode()).hexdigest()
-    db.users.insert_one({'id': id, 'pw': pw_hash})
-
-    return jsonify({'result':'success'})
-
-
-@app.route('/user', methods=["POST"])
-def user_info():
-    token_receive = request.headers['authorization']
-    token = token_receive.split()[1]
-
-    payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-
-    try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-        print(payload)
-        return jsonify({'result' : 'success', 'id': payload['id']})
-    except jwt.exceptions.ExpiredSignatureError:
-        # try부분을 실행했지만 위와 같은 에러가 발생했다면
-        return jsonify({'result' : 'fail'})
-
-
 # app.py 파일을 직접 실행시킬 때 동작시킴
 # 이 코드가 가장 아랫부분
 if __name__ == '__main__':
     app.run(
         '0.0.0.0',  # 모든 IP에서 오는 요청을 허용
         7000,  # 플라스크 웹 서버는 7000번 포트 사용
-         debug=True,  # 에러 발생 시 에러 로그여줌
+        debug=True,  # 에러 발생 시 에러 로그 보여줌
     )
