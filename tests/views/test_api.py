61e575c97822b94eeb83c0e768d4c5ba5c2e75b1
Heeeon <bbjoite09@naver.com>
2021-05-22 13:39:28 +0900

    Test api 로그인, 회원가입 완료

--- tests/views/test_api.py
+++ tests/views/test_api.py
@@ -1,31 +1,50 @@
 import hashlib
 
+import jwt
+
 from tests.conftest import db
 
 
 def test_로그인(client):
-    id = "test05"
-    pw = "test05"
-
     data = {
-        'id': id,
-        'pw': pw,
+        'id_give': 'tester02',
+        'pw_give': 'test'
     }
+    # 먼저 회원가입
+    client.post('/api/register', data=data)
+
+    # 로그인
+    response = client.post(
+        '/api/login',
+        data=data
+    )
+
+    assert response.status_code == 200
+    assert response.json['result'] == 'success'
+
+    token = response.json['token']
+    payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+    assert payload['id'] == 'tester02'
+
 
 def test_회원가입(client):
     data = {
-        'id_give' : "test05",
-        'pw_give': "test05"
+        'id_give': 'tester01',
+        'pw_give': 'test'
     }
 
     response = client.post(
-        '/api/reqisor',
+        '/api/register',
         data=data
     )
 
     assert response.status_code == 200
 
-    user = db.users.find_one({'id' : "test05"}, {'_id' : False})
-
+    user = db.users.find_one(
+        {'id': 'tester01'},
+        {'_id': False}
+    )
+    # 패스워드 평문저장하지 않음
     assert user['pw'] != 'test'
-    hashlib.sha256('test'.encode()).hexdigest()
+    pw_hash = hashlib.sha256('test'.encode()).hexdigest()
+    assert user['pw'] == pw_hash
